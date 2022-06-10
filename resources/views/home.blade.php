c4716b42eda0c34f9a28fbebb4897fc7b64592d0
YChieh <cvop5107@gmail.com>
2022-06-10 21:38:14 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+    <h3>Products</h3>
+@endsection
+
+@section('my_menu')
+    <li class="nav-item">
+        <a class="nav-link" href="/home">主頁</a>
+    </li>
+@stop
